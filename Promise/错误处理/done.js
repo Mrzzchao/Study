9c4165fadc54f1ed43f25fb9d03f03aad218b36f
MChao <mrzzchao@foxmail.com>
2018-11-19 22:49:49 +0800

    学习

---
+++ Promise/错误处理/done.js
@@ -0,0 +1,10 @@
+var p = Promise.resolve(42);
+
+p.then(function(msg) {
+    console.log(msg.toLowerCase());  // 会报错
+}).done(null,handlerErrors);
+
+
+function handlerErrors(err) {
+    console.log(err)
+}
