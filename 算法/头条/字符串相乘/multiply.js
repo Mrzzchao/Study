798a44a5247b1554a9e4bee9375342849f7a916c
zhichazhang <zhichazhang@tencent.com>
2020-05-14 00:53:52 +0800

    feat: 简化路径

---
+++ 算法/头条/字符串相乘/multiply.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var multiply = function(num1, num2) {
+    
+};
+
+const s1 = "prosperity"
+const s2 = "properties"
+const result = multiply(s1, s2)
+console.log(result)
