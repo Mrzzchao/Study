3b9cd4322ed7ecf866e49cd5e477e14f1ba8a307
Mrzzchao <mrzzhichao@gmail.com>
2020-06-09 00:46:11 +0800

    feat: update

---
+++ 算法/头条/两数相加/addTwoNumbers.js
@@ -0,0 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+    let node = new ListNode('head')
+    let tmp = node
+
+    let add = 0
+    let sum = 0;
+
+    while(l1 || l2) {
+        sum = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + add
+
+        tmp.next = new ListNode(sum % 10)
+
+        tmp = tmp.next
+        add = sum >= 10 ? 1 : 0
+
+        l1 && (l1 = l1.next)
+        l2 && (l2 = l2.next)
+    }
+
+    add && (tmp.next = new ListNode(add))
+
+    return node.next
+};
+
+function ListNode(val, next) {
+    this.val = (val===undefined ? 0 : val)
+    this.next = (next===undefined ? null : next)
+}
+
+const n1 = new ListNode(5, null)
+const n2 = new ListNode(4, n1)
+const n3 = new ListNode(2, n2)
+
+
+const n4 = new ListNode(5, null)
+const n5 = new ListNode(6, n4)
+const n6 = new ListNode(5, n5)
+
+const result = addTwoNumbers(n1, n4)
+console.log(result)
