7e7caf2d26e39d5f3d742953448777f25ca230c4
Mrzzchao <mrzzhichao@gmail.com>
2021-01-26 23:46:11 +0800

    feat: update

--- Vue/源码/examples/响应式/index.js
+++ Vue/源码/examples/响应式/index.js
@@ -1,14 +1,15 @@
-var app = new Vue({
-    el: '#app',
-    data: {
-        message: 'Hello Vue!'
+// eslint-disable-next-line
+const app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!',
+  },
+  mounted() {
+    console.log(this.$data);
+  },
+  methods: {
+    changeMsg() {
+      this.message = 'Hello World!';
     },
-    mounted() {
-        console.log(this.$data)
-    },
-    methods: {
-        changeMsg() {
-            this.message = 'Hello World!'
-        }
-    }
-})
+  },
+});
