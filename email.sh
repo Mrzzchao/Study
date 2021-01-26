a65337d56b874e6ed078289195f40f9c3c48d780
Mrzzchao <mrzzhichao@gmail.com>
2021-01-26 23:38:59 +0800

    feat: update

--- email.sh
+++ email.sh
@@ -3,14 +3,13 @@
 git filter-branch --env-filter '
 
 OLD_EMAIL="zhichazhang@tencent.com"
-
 CORRECT_NAME="Mrzzchao"
 CORRECT_EMAIL="mrzzhichao@gmail.com"
 
 if [ "$GIT_COMMITTER_EMAIL" = "$CORRECT_EMAIL" ]
+then
     export GIT_COMMITTER_NAME="$CORRECT_NAME"
     export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
-then
 fi
 if [ "$GIT_AUTHOR_EMAIL" = "$CORRECT_EMAIL" ]
 then
