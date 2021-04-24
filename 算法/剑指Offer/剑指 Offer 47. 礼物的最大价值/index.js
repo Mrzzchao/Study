c9ebd9635394607b302c16b449593e07360290de
Mrzzchao <mrzzhichao@gmail.com>
2021-04-25 00:49:26 +0800

    feat: 剑指 Offer 47. 礼物的最大价值

--- 算法/剑指Offer/剑指 Offer 47. 礼物的最大价值/index.js
+++ 算法/剑指Offer/剑指 Offer 47. 礼物的最大价值/index.js
@@ -9,4 +9,31 @@
  * dp[0 - i][0] = sum(grid[0...i][0]) 左边的每一个点都等于上面点的和
  * dp[0][0...j] = sum(grid[0][0...j]) 上边的每一个点都等于左面点的和
  */
-const maxValue = function (grid) {};
+const maxValue = function (grid) {
+  const m = grid.length;
+  const n = grid[0].length;
+
+  // 直接两个new的话会有同一个引用的问题
+  const dp = Array.from(new Array(m), () => new Array(n).fill(0));
+  dp[0][0] = grid[0][0];
+  for (let i = 1; i < m; i++) {
+    dp[i][0] = grid[i][0] + dp[i - 1][0];
+  }
+  for (let j = 1; j < n; j++) {
+    dp[0][j] = grid[0][j] + dp[0][j - 1];
+  }
+
+  for (let i = 1; i < m; i++) {
+    for (let j = 1; j < n; j++) {
+      dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+    }
+  }
+
+  return dp[m - 1][n - 1];
+};
+
+maxValue([
+  [1, 3, 1],
+  [1, 5, 1],
+  [4, 2, 1],
+]);
