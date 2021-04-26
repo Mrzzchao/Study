fcc1d175c088662a0f44d12e28cc30157a369e6d
Mrzzchao <mrzzhichao@gmail.com>
2021-04-26 23:58:35 +0800

    feat: 剑指 Offer 62. 圆圈中最后剩下的数字

--- 算法/剑指Offer/剑指 Offer 62. 圆圈中最后剩下的数字/index.js
+++ 算法/剑指Offer/剑指 Offer 62. 圆圈中最后剩下的数字/index.js
@@ -2,5 +2,12 @@
  * @param {number} n
  * @param {number} m
  * @return {number}
+ * TODO: 没看懂...
  */
-const lastRemaining = function (n, m) {};
+const lastRemaining = function (n, m) {
+  let f = 0;
+  for (let i = 2; i <= n; i++) {
+    f = (m + f) % i;
+  }
+  return f;
+};
