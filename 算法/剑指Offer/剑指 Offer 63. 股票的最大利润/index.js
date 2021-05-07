1b3f98e628b0f48a559a32d15b519534e6fc4637
Mrzzchao <mrzzhichao@gmail.com>
2021-05-07 23:15:04 +0800

    feat: 剑指 Offer 63. 股票的最大利润

--- 算法/剑指Offer/剑指 Offer 63. 股票的最大利润/index.js
+++ 算法/剑指Offer/剑指 Offer 63. 股票的最大利润/index.js
@@ -2,4 +2,19 @@
  * @param {number[]} prices
  * @return {number}
  */
-const maxProfit = function (prices) {};
+const maxProfit = function (prices) {
+  // 最大利润
+  let max = 0;
+
+  // 最小值
+  let min = prices[0];
+
+  for (let i = 1; i < prices.length; i++) {
+    max = Math.max(max, prices[i] - min);
+    if (prices[i] < min) {
+      min = prices[i];
+    }
+  }
+
+  return max;
+};
