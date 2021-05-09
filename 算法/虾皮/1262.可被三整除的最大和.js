c2ead882b63d7ca0344de5810abc8adf12472d29
Mrzzchao <mrzzhichao@gmail.com>
2021-05-09 23:55:57 +0800

    feat: update

---
+++ 算法/虾皮/1262.可被三整除的最大和.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=1262 lang=javascript
+ *
+ * [1262] 可被三整除的最大和
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ * 动态规划
+ * dp[i][j]:以nums[i...j]范围的最大和
+ * dp[i][j]=
+ */
+const maxSumDivThree = function (nums) {};
+// @lc code=end
