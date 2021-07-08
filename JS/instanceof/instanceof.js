68c650b1a984d6225a0a934a49bd9dfe874e0b4b
Mrzzchao <mrzzhichao@gmail.com>
2021-07-09 00:02:46 +0800

    feat: instanceof

--- JS/instanceof/instanceof.js
+++ JS/instanceof/instanceof.js
@@ -1,16 +1,23 @@
+// 含义：a instanceof b 代表判断a的原型链向上是不是能找到b
 function myInstanceof(left, right) {
+  // 获取类型原型
   const { prototype } = right;
+
+  // 获取对象原型
   left = left.__proto__;
 
   while (true) {
+    // null 没有原型
     if (left === null) {
       return false;
     }
     if (left === prototype) {
       return true;
     }
+
+    // 一直沿着原型向上
     left = left.__proto__;
   }
 }
 
-console.log(myInstanceof([], String));
+console.log(myInstanceof([], Array));
